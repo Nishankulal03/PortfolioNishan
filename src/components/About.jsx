61bc519a1ce1aead86c98251265c782f1ee1e813
Nishan kulal <109814884+Nishankulal03@users.noreply.github.com>
2025-03-15 16:25:44 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
         </p>
         <br/>
         <button className="mx-auto mt-4 px-6 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 flex">
-          <a href="https://drive.google.com/file/d/1oW5ESQm-HYHHDA9ssHD0SKyVKXRtKOkC/view?usp=sharing" className="w-full text-center">Download CV</a>
+          <a href="https://drive.google.com/file/d/1TFOWm6b2CYtgaSy6Tk_Cqc33Tj2hJu4a/view?usp=sharing" className="w-full text-center">Download CV</a>
         </button>
       </div>
     </section>
