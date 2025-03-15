476735ab96bc7d7194113ebadea11e8a4df129ba
Nishan kulal <109814884+Nishankulal03@users.noreply.github.com>
2025-03-15 16:38:07 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,8 @@ import p2 from '../assets/boo.jpeg';
 import p3 from '../assets/disne.jpeg';
 import p4 from '../assets/ap.jpeg';
 import p5 from '../assets/imag.jpeg';
-import p6 from '../assets/ai.jpeg';
+import p6 from '../assets/ai.png
+  ';
 import { FaLink, FaGithub } from "react-icons/fa";
 
 // Sample portfolio data
